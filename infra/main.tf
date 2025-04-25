ce59d53930f634519aaa235b599a03f5d01ff620
mandresuri <mandresuri@gmail.com>
2025-04-25 01:10:00 -0500

    feat(#3): deploy gateway
    - deploy gateway
    - post endpoint register

--- infra/main.tf
+++ infra/main.tf
@@ -98,3 +98,22 @@ resource "aws_lambda_function" "register_user" {
     }
   }
 }
+
+# api gateway
+resource "aws_api_gateway_rest_api" "register_api" {
+  name        = "register-api"
+  description = "API for registering user"
+}
+
+resource "aws_api_gateway_resource" "register_resource" {
+  rest_api_id = aws_api_gateway_rest_api.register_api.id
+  parent_id   = aws_api_gateway_rest_api.register_api.root_resource_id
+  path_part   = "register"
+}
+
+resource "aws_api_gateway_method" "register_method" {
+  rest_api_id   = aws_api_gateway_rest_api.register_api.id
+  resource_id   = aws_api_gateway_resource.register_resource.id
+  http_method   = "POST"
+  authorization = "NONE"
+}
